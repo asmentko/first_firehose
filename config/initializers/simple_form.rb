16ce7dbbe148e9f74584a440485b38ac9f335506
asmentko <asmentko@gmail.com>
2016-11-04 03:40:39 +0000

    Removed code that got rid of the asterix

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -163,7 +163,4 @@ SimpleForm.setup do |config|
 
   # Defines which i18n scope will be used in Simple Form.
   # config.i18n_scope = 'simple_form'
-
-  #remove asterix in required fields
-  config.label_text = lambda { |label, placeholder, required| "#{label}" }
 end
